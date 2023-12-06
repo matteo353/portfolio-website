8f36aefa50e8cfb83110537274c2883d2df97399
Matteo Mastandrea <matteomastandrea@Matteos-MBP.hsd1.ma.comcast.net>
2023-12-06 17:34:59 -0500

    hamburger menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,25 @@
-import React from 'react'
+import React , {useState } from 'react'
 import './Styling/Navbar.css'
 
 function Navbar({scrollToAbout, scrollToProjects, scrollToSkills, scrollToWork}) {
   const resumeUrl = "https://drive.google.com/file/d/1ySKFYyeiMikuqCKIgd59ibW0UkMeLDb3/view?usp=sharing";
+  const [isActive, setIsActive] = useState(false);
 
 
   const viewResume = () => {
       window.open(resumeUrl, '_blank', 'noopener,noreferrer');
   };
+
+  const hamburgerMenuClick = () => {
+    setIsActive(current => !current); // Toggles the isActive state
+  };
+
+
+
+
   return (
     <div>
+    <div className='nav'>
         <nav className="navbar">
         <ul className="nav-list">
         <li className="nav-item" onClick={scrollToAbout}>ABOUT ME</li>
@@ -19,7 +29,27 @@ function Navbar({scrollToAbout, scrollToProjects, scrollToSkills, scrollToWork})
         <li className="nav-item" onClick={viewResume}>RESUME</li>
       </ul>
     </nav>
+    <button
+      className={`hamburger ${isActive ? 'is-active' : ''}`}
+      onClick={hamburgerMenuClick}
+     >
+      <div className='bar'></div>
+    </button>
     </div>
+    <nav className={`mobile-nav ${isActive ? 'is-active' : ''}`}>
+    <ul className="mobile-nav-list" onClick={hamburgerMenuClick}>
+        <li onClick={scrollToAbout }>ABOUT ME</li>
+        <li onClick={scrollToProjects}>PROJECTS</li>
+        <li onClick={scrollToSkills}>SKILLS</li>
+        <li onClick={scrollToWork}>WORK HISTORY</li>
+        <li onClick={viewResume}>RESUME</li>
+      </ul>
+
+    </nav>
+
+
+    </div>
+    
   )
 }
 
