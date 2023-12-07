9f566b41296bbcf6ee8c2ee661b520a1555f5784
Matteo Mastandrea <matteomastandrea@Matteos-MBP.hsd1.ma.comcast.net>
2023-12-06 20:04:17 -0500

    about paragraph update

--- src/components/About.js
+++ src/components/About.js
@@ -16,7 +16,7 @@ function About() {
           <div className="text-box">
             <div className='about-details'>
             <h1 className="about-title">About Me</h1>
-            <p>Hello! I'm a recent Computer Science graduate from UMass Amherst, where I honed my skills with a particular focus on data science. I enjoy making aesthetic user interfaces, building web applications, and all things data. My academic journey and past professional experiences have armed me with the tools to develop user-centric software solutions and translate intricate data-driven concepts into effective business strategies. Eager to apply my data science and programming expertise in a dynamic professional setting, I am looking forward to collaborating on projects that push the boundaries of what's possible in tech and business.</p>
+            <p>Hello! I am a recent Computer Science graduate from UMass Amherst with a particular focus on data science. I excel at data analytics, building web applications, and especially enjoy designing user interfaces. Additionally, my academic and professional journey has given me extensive exposure to database management and cloud technologies, particularly AWS. Outside of technology, I love to watch movies, ski, golf, and play basketball. </p>
               <div className="social-icons">
               <button className='resume-button' onClick={viewResume} ><span>View Resume</span></button>
               <a href="https://linkedin.com/in/matteo3/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
